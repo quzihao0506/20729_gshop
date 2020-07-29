fa7238cc2b1228dc216d88c6ca4dce470c2b0a5f
屈子豪 <7688117+q_zh@user.noreply.gitee.com>
2020-07-29 22:07:53 +0800

    init gshop

---
+++ src/pages/Msite/Msite.vue
@@ -0,0 +1,28 @@
+<template>
+
+</template>
+
+<script>
+import HeaderTop from '../../components/HeaderTop/HeaderTop.vue'
+import Swiper from 'swiper'
+import 'swiper/dist/css/swiper.min.css'
+export default {
+    mounted () {
+        // 创建一个Swiper实例对象
+        new Swiper('.swiper-container', {
+            loop: true, //循环轮播
+            // 如果需要分页器
+            pagination: {
+            el: '.swiper-pagination',
+            },
+        })
+    },
+    components: {
+        HeaderTop
+    }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
